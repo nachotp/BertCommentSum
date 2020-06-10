50986668c251b764bfd5738fd7f7bdf2dae0ec0d
Ignacio Tampe <thenachotp@gmail.com>
2020-06-10 03:00:19 -0300

    First push from fork

---
+++ src/models/model_builder.py
@@ -0,0 +1,196 @@
+import copy
+
+import torch
+import torch.nn as nn
+from transformers import BertModel, BertConfig
+from torch.nn.init import xavier_uniform_
+
+from models.decoder import TransformerDecoder
+from models.encoder import Classifier, ExtTransformerEncoder
+from models.optimizers import Optimizer
+
+def build_optim(args, model, checkpoint):
+    """ Build optimizer """
+
+    if checkpoint is not None:
+        optim = checkpoint['optim'][0]
+        saved_optimizer_state_dict = optim.optimizer.state_dict()
+        optim.optimizer.load_state_dict(saved_optimizer_state_dict)
+        if args.visible_gpus != '-1':
+            for state in optim.optimizer.state.values():
+                for k, v in state.items():
+                    if torch.is_tensor(v):
+                        state[k] = v.cuda()
+
+        if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
+            raise RuntimeError(
+                "Error: loaded Adam optimizer from existing model" +
+                " but optimizer state is empty")
+
+    else:
+        optim = Optimizer(
+            args.optim, args.lr, args.max_grad_norm,
+            beta1=args.beta1, beta2=args.beta2,
+            decay_method='noam',
+            warmup_steps=args.warmup_steps)
+
+    optim.set_parameters(list(model.named_parameters()))
+
+
+    return optim
+
+def build_optim_bert(args, model, checkpoint):
+    """ Build optimizer """
+
+    if checkpoint is not None:
+        optim = checkpoint['optims'][0]
+        saved_optimizer_state_dict = optim.optimizer.state_dict()
+        optim.optimizer.load_state_dict(saved_optimizer_state_dict)
+        if args.visible_gpus != '-1':
+            for state in optim.optimizer.state.values():
+                for k, v in state.items():
+                    if torch.is_tensor(v):
+                        state[k] = v.cuda()
+
+        if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
+            raise RuntimeError(
+                "Error: loaded Adam optimizer from existing model" +
+                " but optimizer state is empty")
+
+    else:
+        optim = Optimizer(
+            args.optim, args.lr_bert, args.max_grad_norm,
+            beta1=args.beta1, beta2=args.beta2,
+            decay_method='noam',
+            warmup_steps=args.warmup_steps_bert)
+
+    params = [(n, p) for n, p in list(model.named_parameters()) if n.startswith('bert.model')]
+    optim.set_parameters(params)
+
+
+    return optim
+
+def build_optim_dec(args, model, checkpoint):
+    """ Build optimizer """
+
+    if checkpoint is not None:
+        optim = checkpoint['optims'][1]
+        saved_optimizer_state_dict = optim.optimizer.state_dict()
+        optim.optimizer.load_state_dict(saved_optimizer_state_dict)
+        if args.visible_gpus != '-1':
+            for state in optim.optimizer.state.values():
+                for k, v in state.items():
+                    if torch.is_tensor(v):
+                        state[k] = v.cuda()
+
+        if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
+            raise RuntimeError(
+                "Error: loaded Adam optimizer from existing model" +
+                " but optimizer state is empty")
+
+    else:
+        optim = Optimizer(
+            args.optim, args.lr_dec, args.max_grad_norm,
+            beta1=args.beta1, beta2=args.beta2,
+            decay_method='noam',
+            warmup_steps=args.warmup_steps_dec)
+
+    params = [(n, p) for n, p in list(model.named_parameters()) if not n.startswith('bert.model')]
+    optim.set_parameters(params)
+
+
+    return optim
+
+
+def get_generator(vocab_size, dec_hidden_size, device):
+    gen_func = nn.LogSoftmax(dim=-1)
+    generator = nn.Sequential(
+        nn.Linear(dec_hidden_size, vocab_size),
+        gen_func
+    )
+    generator.to(device)
+
+    return generator
+
+class Bert(nn.Module):
+    def __init__(self, temp_dir, cased=False, finetune=False):
+        super(Bert, self).__init__()
+        if(cased):
+            self.model = BertModel.from_pretrained('bert_data/cased/', cache_dir=temp_dir)
+        else:
+            self.model = BertModel.from_pretrained('bert_data/uncased/', cache_dir=temp_dir)
+
+        self.finetune = finetune
+
+    def forward(self, x, segs, mask):
+        if(self.finetune):
+            top_vec, _ = self.model(x, segs, attention_mask=mask)
+        else:
+            self.eval()
+            with torch.no_grad():
+                top_vec, _ = self.model(x, segs, attention_mask=mask)
+        return top_vec
+
+class AbsSummarizer(nn.Module):
+    def __init__(self, args, device, checkpoint=None):
+        super(AbsSummarizer, self).__init__()
+        self.args = args
+        self.device = device
+        self.bert = Bert(args.large, args.temp_dir, args.finetune_bert)
+
+        if (args.encoder == 'baseline'):
+            bert_config = BertConfig(self.bert.model.config.vocab_size, hidden_size=args.enc_hidden_size,
+                                     num_hidden_layers=args.enc_layers, num_attention_heads=8,
+                                     intermediate_size=args.enc_ff_size,
+                                     hidden_dropout_prob=args.enc_dropout,
+                                     attention_probs_dropout_prob=args.enc_dropout)
+            self.bert.model = BertModel(bert_config)
+
+        if(args.max_pos>512):
+            my_pos_embeddings = nn.Embedding(args.max_pos, self.bert.model.config.hidden_size)
+            my_pos_embeddings.weight.data[:512] = self.bert.model.embeddings.position_embeddings.weight.data
+            my_pos_embeddings.weight.data[512:] = self.bert.model.embeddings.position_embeddings.weight.data[-1][None,:].repeat(args.max_pos-512,1)
+            self.bert.model.embeddings.position_embeddings = my_pos_embeddings
+        self.vocab_size = self.bert.model.config.vocab_size
+        tgt_embeddings = nn.Embedding(self.vocab_size, self.bert.model.config.hidden_size, padding_idx=0)
+        if (self.args.share_emb):
+            tgt_embeddings.weight = copy.deepcopy(self.bert.model.embeddings.word_embeddings.weight)
+
+        self.decoder = TransformerDecoder(
+            self.args.dec_layers,
+            self.args.dec_hidden_size, heads=self.args.dec_heads,
+            d_ff=self.args.dec_ff_size, dropout=self.args.dec_dropout, embeddings=tgt_embeddings)
+
+        self.generator = get_generator(self.vocab_size, self.args.dec_hidden_size, device)
+        self.generator[0].weight = self.decoder.embeddings.weight
+
+
+        if checkpoint is not None:
+            self.load_state_dict(checkpoint['model'], strict=True)
+        else:
+            for module in self.decoder.modules():
+                if isinstance(module, (nn.Linear, nn.Embedding)):
+                    module.weight.data.normal_(mean=0.0, std=0.02)
+                elif isinstance(module, nn.LayerNorm):
+                    module.bias.data.zero_()
+                    module.weight.data.fill_(1.0)
+                if isinstance(module, nn.Linear) and module.bias is not None:
+                    module.bias.data.zero_()
+            for p in self.generator.parameters():
+                if p.dim() > 1:
+                    xavier_uniform_(p)
+                else:
+                    p.data.zero_()
+            if(args.use_bert_emb):
+                tgt_embeddings = nn.Embedding(self.vocab_size, self.bert.model.config.hidden_size, padding_idx=0)
+                tgt_embeddings.weight = copy.deepcopy(self.bert.model.embeddings.word_embeddings.weight)
+                self.decoder.embeddings = tgt_embeddings
+                self.generator[0].weight = self.decoder.embeddings.weight
+
+        self.to(device)
+
+    def forward(self, src, tgt, segs, clss, mask_src, mask_tgt, mask_cls):
+        top_vec = self.bert(src, segs, mask_src)
+        dec_state = self.decoder.init_decoder_state(src, top_vec)
+        decoder_outputs, state = self.decoder(tgt[:, :-1], top_vec, dec_state)
+        return decoder_outputs, None
