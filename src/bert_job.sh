1cd6e9eb0793871d34cc8e3900eed1010052cf0d
Ignacio Tampe <nachotp@gra-login2.graham.sharcnet>
2020-06-15 17:16:19 -0400

    Updates on job scheduler

--- src/bert_job.sh
+++ src/bert_job.sh
@@ -11,4 +11,4 @@ export CLASSPATH=~/projects/def-emilios/nachotp/stanford-corenlp-4.0.0/stanford-
 export CLASSPATH=$CLASSPATH:~/projects/def-emilios/nachotp/BertCommentSum/stanford-corenlp-4.0.0-models-spanish.jar
 
 lemiknow telegram --token 1081026745:AAG11u-kKTPlEqnmYvmdXsnMLVDlah-8kTI --chat-id 178174272 --include_details True \
-    python src/train.py  -task abs -mode train -bert_data_path BERT_DATA_PATH -dec_dropout 0.2  -model_path MODEL_PATH -sep_optim true -lr_bert 0.002 -lr_dec 0.2 -save_checkpoint_steps 2000 -batch_size 140 -train_steps 200000 -report_every 50 -accum_count 5 -use_bert_emb true -use_interval true -warmup_steps_bert 20000 -warmup_steps_dec 10000 -max_pos 512 -visible_gpus -1  -log_file logs/abs_bert_cnndm
+    python train.py  -mode train -dec_dropout 0.2  -sep_optim true -lr_bert 0.002 -lr_dec 0.2 -save_checkpoint_steps 2000 -batch_size 140 -train_steps 200000 -report_every 50 -accum_count 5 -use_bert_emb true -use_interval true -warmup_steps_bert 20000 -warmup_steps_dec 10000 -max_pos 512 -visible_gpus 0  -log_file logs/demo1
--- src/train.py
+++ src/train.py
@@ -29,10 +29,10 @@ if __name__ == '__main__':
     parser.add_argument("-task", default='abs', type=str, choices=['abs'])
     parser.add_argument("-encoder", default='bert', type=str, choices=['bert', 'baseline'])
     parser.add_argument("-mode", default='train', type=str, choices=['train', 'validate', 'test'])
-    parser.add_argument("-bert_data_path", default='../bert_data_new/cnndm')
+    parser.add_argument("-bert_data_path", default='../bert_data/')
     parser.add_argument("-model_path", default='../models/')
-    parser.add_argument("-result_path", default='../results/cnndm')
-    parser.add_argument("-temp_dir", default='../temp')
+    parser.add_argument("-result_path", default='../results/')
+    parser.add_argument("-temp_dir", default='../temp/')
 
     parser.add_argument("-batch_size", default=140, type=int)
     parser.add_argument("-test_batch_size", default=200, type=int)
